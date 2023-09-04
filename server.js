4c1dd32a160696e5eb59d54558fe873c06b4eebf
Syed Javith <syedjavith14@gmail.com>
2023-09-04 23:07:53 +0530

    final

--- server.js
+++ server.js
@@ -107,7 +107,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/oauth",
+    callbackURL: "https://rsjlmsnode.onrender.com/auth/google/oauth",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
